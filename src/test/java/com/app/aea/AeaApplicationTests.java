25d7f36926fda1c27f163443fb646b78777a49aa
ascaryaaa <allaamascarya@gmail.com>
2024-02-23 13:17:19 +0700

    initial commit

---
+++ src/test/java/com/app/aea/AeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.aea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
